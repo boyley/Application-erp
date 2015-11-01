914127d72f87268c09cf16ea5c739e170de44395
Boyley <274163579@qq.com>
2015-11-01 16:09:04 +0800

    api整理

--- standard/erp-front/src/main/java/com/github/application/erp/controller/LedOrderController.java
+++ standard/erp-front/src/main/java/com/github/application/erp/controller/LedOrderController.java
@@ -3,6 +3,7 @@ package com.github.application.erp.controller;
 import com.github.application.erp.controller.search.QueryProduce;
 import com.github.application.erp.entity.Product;
 import com.github.application.erp.service.ProductService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,14 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(value = "order")
+@io.swagger.annotations.Api(value = "LED订单管理")
 public class LedOrderController {
 
     @Autowired
     private ProductService productService;
 
     @RequestMapping(value = "/publish")
+    @ApiOperation(value = "添加订单页面")
     public ModelAndView publish() {
         List<Product> products = productService.find(new QueryProduce());
         ModelAndView modelAndView = new ModelAndView("admin/order/led-order-edit");
