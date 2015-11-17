3e71e397f2175c35260d67196deac2db5c4de1bb
Boyley <274163579@qq.com>
2015-11-17 16:28:21 +0800

    订单管理

--- standard/erp-front/src/main/java/com/github/application/erp/controller/LedOrderController.java
+++ standard/erp-front/src/main/java/com/github/application/erp/controller/LedOrderController.java
@@ -1,10 +1,12 @@
 package com.github.application.erp.controller;
 
 import com.github.application.erp.controller.search.QueryOrder;
+import com.github.application.erp.controller.search.QueryProduce;
 import com.github.application.erp.controller.web.Api;
 import com.github.application.erp.entity.Order;
 import com.github.application.erp.entity.Product;
 import com.github.application.erp.service.OrderService;
+import com.github.application.erp.service.ProductService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Created by 赵波 on 2015/10/28.
@@ -28,6 +31,9 @@ public class LedOrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ProductService productService;
+
     @ApiOperation(value = "分页获取LED订单列表信息")
     @RequestMapping(value = "/list", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<Order> list(@ApiParam(required = true, value = "分页信息") Pageable pageable, @ApiParam(required = false, value = "查询条件") QueryOrder queryOrder) {
@@ -38,7 +44,7 @@ public class LedOrderController {
     @ApiOperation(value = "修改LED产品")
     @RequestMapping(value = "/edit", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
     public ModelAndView edit(@ApiParam(required = true, value = "需要修改的LED订单ID号") @RequestParam("id") Long id) {
-        ModelAndView modelAndView = new ModelAndView("admin/order/led-order-edit");
+        ModelAndView modelAndView = new ModelAndView("/admin/order/led-order-edit");
         Order order = this.orderService.selectByPrimaryKey(id);
         modelAndView.addObject(order);
         return modelAndView;
@@ -47,7 +53,9 @@ public class LedOrderController {
     @ApiOperation(value = "添加LED订单页面")
     @RequestMapping(value = "/publish", method = RequestMethod.GET)
     public ModelAndView publish() {
-        ModelAndView modelAndView = new ModelAndView("admin/order/led-order-edit");
+        List<Product> products = productService.find(new QueryProduce());
+        ModelAndView modelAndView = new ModelAndView("/admin/order/led-order-edit");
+        modelAndView.addObject("products", products);
         return modelAndView;
     }
 
